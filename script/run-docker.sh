881dce43dd5d29a610b583ba44b2201faf474ab2
Tsubasa Takayama <t-takayama@feedforce.jp>
2018-08-27 16:03:20 +0900

    Avoid redundant directory in docker cp
    e.g. `/root/project/dist/dist/nginx-1.8.1-ff1.el7.ngx.x86_64.rpm`
    
    ref: https://circleci.com/gh/feedforce/nginx-headers-more-rpm/59

--- script/run-docker.sh
+++ script/run-docker.sh
@@ -6,5 +6,5 @@ CENTOS_MAJOR_VERSION=$1
 DOCKER_IMAGE=centos${CENTOS_MAJOR_VERSION}/nginx-rpm
 
 docker run -u builder --name builder $DOCKER_IMAGE ./build-nginx.sh
-docker cp builder:/home/builder/dist $CIRCLE_WORKING_DIRECTORY/dist
+docker cp builder:/home/builder/dist $CIRCLE_WORKING_DIRECTORY/
 docker rm builder
